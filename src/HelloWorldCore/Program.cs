247451007eddc1b2a8f89ac70154f617aa6df436
michaelcgi <michael.ahs@cgi.com>
2016-08-11 13:03:46 +0200

    Add project files.

---
+++ src/HelloWorldCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello world! Time is: " +
+                DateTime.Now.ToString("t"));
+        }
+    }
+}
---
+++ src/StartProcessCore/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartProcessCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args.Length == 0 || args.Length > 2)
+            {
+                Console.Out.WriteLine("StartProcessCore [fileName] <arguments>");
+                return;
+            }
+
+            var psi = CreateProcessStartInfo(args);
+            var p = Process.Start(psi);
+
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.Out.WriteLine("OUTPUT:");
+            Console.Out.WriteLine(output);
+        }
+
+        private static ProcessStartInfo CreateProcessStartInfo(string[] args)
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = args[0];
+            if (args.Length > 1)
+            {
+                psi.Arguments = args[1];
+            }
+
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            return psi;
+        }
+    }
+}
